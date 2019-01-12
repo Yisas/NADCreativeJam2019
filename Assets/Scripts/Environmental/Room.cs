8ece55a971c2f6a852038ad00e2eb730fe259587
Jesus Imery <yisas_0929@hotmail.com>
2019-01-12 09:31:47 -0500

    #6 Respawn locations based on room zones

---
+++ Assets/Scripts/Environmental/Room.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Room : MonoBehaviour {
+
+    public Transform playerRespawnLocation;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            FindObjectOfType<GameController>().SetActiveRoom(this);
+        }
+    }
+
+    public void RespawnPlayer(Transform playerTransform)
+    {
+        playerTransform.position = playerRespawnLocation.position;
+    }
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+    public Transform player;
+    private Room activeRoom;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// DELETEME
+        if(Input.GetButtonDown("Test Button"))
+        {
+            RespawnPlayer();
+        }
+	}
+
+    public void RespawnPlayer()
+    {
+        activeRoom.RespawnPlayer(player);
+    }
+
+    public void SetActiveRoom(Room room)
+    {
+        activeRoom = room;
+    }
+}
