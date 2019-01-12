bcc8b6369cc4efcab2b228e94048b6daf924db0b
anthony <dubois.anthony@hotmail.com>
2019-01-12 12:52:35 -0500

    #8 Script for vision detection

--- Assets/Scripts/AI Character/SeekingPlayerVision.cs
+++ Assets/Scripts/AI Character/SeekingPlayerVision.cs
@@ -2,15 +2,58 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+using UnityEngine.AI;
+
 public class SeekingPlayerVision : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+    public GameObject Player;
+    public float timer;
+    public bool PlayerVisible;
+    public bool GetPlayer;
+
+    private NavMeshAgent agent;
+
+    // Use this for initialization
+    void Start () {
+        if(Player == null)
+        {
+            Player = Player = GameObject.FindGameObjectWithTag("Player");
+        }
+        agent = GetComponent<NavMeshAgent>();
+
+        StartCoroutine(seekPlayer());
+
+    }
+
+    IEnumerator seekPlayer()
+    {
+        yield return new WaitForSeconds(timer);
+        if (PlayerVisible)
+        {
+            GetPlayer = true;
+        }
+        //tbd
+        else GetPlayer = false;
+
+    }
+
+    void GotoPlayer()
+    {
+        // Returns if no points have been set up
+        if (Player == null)
+            return;
+
+        // Set the agent to go to the currently selected destination.
+        agent.destination = Player.transform.position;
+
+       
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (GetPlayer)
+        {
+            GotoPlayer();
+        }
 	}
 }
