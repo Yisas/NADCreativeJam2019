205441d5cb096d8dcb19ce3f84ee943ffae8fa79
Jesus Imery <yisas_0929@hotmail.com>
2019-01-12 09:52:28 -0500

    #7 Placeholder noisy object sound

--- Assets/Scripts/Environmental/NoisyObject.cs
+++ Assets/Scripts/Environmental/NoisyObject.cs
@@ -2,13 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(AudioSource))]
 public class NoisyObject : MonoBehaviour {
 
     private PlayerDetection playerDetection;
+    private AudioSource audioSource;
 
 	// Use this for initialization
 	void Start () {
         playerDetection = FindObjectOfType<PlayerDetection>();
+        audioSource = GetComponent<AudioSource>();
 	}
 	
 	// Update is called once per frame
@@ -21,6 +24,8 @@ public class NoisyObject : MonoBehaviour {
         if(collision.transform.tag == "Player")
         {
             playerDetection.MakeCatHearPlayer();
+            if(!audioSource.isPlaying)
+                audioSource.Play();
         }
     }
 }
