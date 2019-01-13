9399ae03b9646613d532fd08e4ebaade481fcb77
Jesus Imery <yisas_0929@hotmail.com>
2019-01-13 11:33:44 -0500

    #16 #12 End screen menu implementation. Awaiting art

---
+++ Assets/Scripts/UI/EndLevelMenu.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndLevelMenu : MonoBehaviour, CanvasCallbackReceiver {
+    public void Execute()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Replay()
+    {
+        GetComponent<CameraCanvas>().FadeOutIn(this);
+    }
+}
