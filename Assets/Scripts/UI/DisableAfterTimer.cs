b459923fca2aa2afffe8ea3618cb3ecf71120c4d
Jesus Imery <yisas_0929@hotmail.com>
2019-01-13 10:38:02 -0500

    #12 Initial tutorial message

---
+++ Assets/Scripts/UI/DisableAfterTimer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisableAfterTimer : MonoBehaviour {
+
+    public float time;
+    private float timer;
+
+	// Use this for initialization
+	void Start () {
+        timer = time;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timer -= Time.deltaTime;
+        if(timer <= 0)
+        {
+            gameObject.SetActive(false);
+        }
+	}
+}
